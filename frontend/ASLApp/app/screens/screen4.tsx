e464615f8e6d7ca7fdddc02d0817952e2888999b
TaikiLowe <taiki.lowe@gmail.com>
2025-04-19 22:18:06 -0700

    a

--- frontend/ASLApp/app/screens/screen4.tsx
+++ frontend/ASLApp/app/screens/screen4.tsx
@@ -1,14 +1,83 @@
-import { View, Text, Button } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 import { useRouter } from 'expo-router';
 
 export default function Screen1() {
-    const router = useRouter();
+  const router = useRouter();
 
-    return (
-        <View style={{ flex: 1, justifyContent: "flex-end", alignItems: "center", paddingBottom: 30 }}>
-            <Text>Welcome!</Text>
-            <Button title="Continue" onPress={() => router.push("/screens/camera")} />
+  return (
+    <View style={styles.container}>
+      <View style={styles.card}>
+        <Text style={styles.title}>Communicate</Text>
+        <Text style={styles.subtitle}>
+          It's that easy! Now you may freely translate ASL.
+        </Text>
+
+        <View style={styles.dotsContainer}>
+          <View style={styles.dot} />
+          <View style={styles.dot} />
+          <View style={styles.dot} /> 
+          <View style={[styles.dot, styles.activeDot]} />
         </View>
-    )
 
+        <Pressable style={styles.button} onPress={() => router.push('/screens/camera')}>
+          <Text style={styles.buttonText}>Let's Translate!</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    backgroundColor: '#ddd',
+  },
+  card: {
+    backgroundColor: 'white',
+    padding: 24,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 8, 
+    textAlign: 'left'
+  },
+  subtitle: {
+    textAlign: 'left',
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+  dotsContainer: {
+    flexDirection: 'row',
+    gap: 8,
+    marginBottom: 24,
+  },
+  dot: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: '#888',
+  },
+  activeDot: {
+    backgroundColor: '#d87aa6',
+  },
+  button: {
+    backgroundColor: '#17376A',
+    borderRadius: 999,
+    paddingVertical: 14,
+    paddingHorizontal: 32,
+    width: '100%',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '600',
+  },
+});
