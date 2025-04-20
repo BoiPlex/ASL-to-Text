28a96c83ddad41fd992d288cc672fcf12dc9b6cb
ProcrastinateGuy <ching91202@gmail.com>
2025-04-19 19:58:50 -0700

    adjust the number verification logic to fit more than 10 labels

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,7 @@ from utils import CvFpsCalc
 from model import KeyPointClassifier
 from model import PointHistoryClassifier
 
-OFFSET = 0 # Current label offset to collect more than just 10 labels
+OFFSET = 10 # Current label offset to collect more than just 10 labels
 # Increment offset by 10
 
 # Read labels ###########################################################
@@ -283,14 +283,14 @@ def pre_process_point_history(image, point_history):
 
 
 def logging_csv(number, mode, landmark_list, point_history_list):
-    if mode == 0:
-        pass
-    if mode == 1 and (0 <= number <= 9):
+    if mode == 0 or number < 0: # number -1 is default, avoid capturing that
+        return
+    if mode == 1:
         csv_path = 'model/keypoint_classifier/keypoint.csv'
         with open(csv_path, 'a', newline="") as f:
             writer = csv.writer(f)
             writer.writerow([number, *landmark_list])
-    if mode == 2 and (0 <= number <= 9):
+    if mode == 2:
         csv_path = 'model/point_history_classifier/point_history.csv'
         with open(csv_path, 'a', newline="") as f:
             writer = csv.writer(f)
@@ -536,7 +536,7 @@ def draw_info(image, fps, mode, number):
         cv.putText(image, "MODE:" + mode_string[mode - 1], (10, 90),
                    cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1,
                    cv.LINE_AA)
-        if 0 <= number <= 9:
+        if number >= 0:
             cv.putText(image, "NUM:" + str(number), (10, 110),
                        cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1,
                        cv.LINE_AA)
